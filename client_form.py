321e1776cc64fb915c310e5b1c4b623739bbf980
= <jord52@gmail.com>
2014-07-23 20:04:25 -0400

    client_thread refactor 	modified:   client_form.py 	new file:   client_thread.py

--- client_form.py
+++ client_form.py
@@ -4,78 +4,7 @@ import sys
 import client_dialog
 import client
 from packet import packet
-
-
-class client_thread(QtCore.QThread):
-
-    workspace_received = QtCore.pyqtSignal(object) #signal for multi-thread support !
-    write_status_changed = QtCore.pyqtSignal(object)
-    write_status_quo = QtCore.pyqtSignal(object)
-
-    def __init__(self, form, hostname, port):
-        QtCore.QThread.__init__(self)
-        self.form = form
-        self.hostname = hostname
-        self.port = port
-        self.client = client.client(self.hostname, self.port, self.form.mode)
-        self.right_updated = None
-        self.client.workspace_received = self.__workspace_received
-        self.client.write_status_changed = self.__write_status_changed
-        self.client.write_status_quo = self.__write_status_quo
-
-    def __write_status_quo(self, can_write):
-        self.write_status_changed.emit(can_write)
-        if not can_write:
-            self.form.append_log("You are on the waiting list for the right")
-        else:
-            print "client form write status quo case not expected value: " + str(can_write)
-
-    def __write_status_changed(self, can_write):
-        self.write_status_changed.emit(can_write)
-        if can_write:
-            print "I can write " + str(can_write)
-            self.form.append_log("You possess the right to write.")
-        else:
-            print "I can't write: " + str(can_write)
-            self.form.append_log("You do not possess the right to write anymore.")
-
-    def __workspace_received(self, workspace):
-        self.workspace_received.emit(workspace)
-
-    def __right_updated(self, enabled):
-        if self.right_updated is not None:
-            self.right_updated(enabled)
-
-    def run(self):
-        if self.client.connect():
-            self.form.append_log("Connected to %s:%d" % (self.hostname, self.port))
-            self.form.lbl_status_value.setText("Connected")
-            self.client.disconnected = self.disconnected
-
-            self.form.btn_write_right.setEnabled(True)
-
-            self.client.loop()
-        else:
-            self.form.append_log("Unable to etablish a connection with %s:%d" % (self.hostname, self.port))
-
-        self.form.lbl_status_value.setText("Not Connected")
-        self.form.connection_thread = None
-
-    def close(self):
-        self.client.close()
-
-    def disconnected(self):
-        self.form.lbl_status_value.setText("Not Connected")
-        self.form.append_log("Disconnected from the server (%s:%d)" % (self.hostname, self.port))
-
-    def send_packet(self, packet):
-        self.client.queued_packets.append(packet)
-
-    def release_right(self):
-        p = packet()
-        p.packet_type = packet.ReleaseRight
-        #todo need to send workspace diff with this
-        self.client.queued_packets.append(p)
+from client_thread import client_thread
 
 
 class client_form(QtGui.QMainWindow):
